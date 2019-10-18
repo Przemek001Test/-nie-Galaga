3868c459c254c80fae51e1269933c2860c297df5
Student <student@swps.edu.pl>
2019-10-18 20:05:20 +0200

    koniec pracy

---
+++ floder wewnentrzny/KinematicBody2D.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+var wektor = Vector2(0, 0)
+var rotacja = 0
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed('ui_up'):
+		pass
+	if Input.is_action_pressed('ui_down'):
+		pass
+	if Input.is_action_pressed('ui_left'):
+		pass
+	if Input.is_action_pressed('ui_right'):
+		pass
+
+func _ready():
+	pass # Replace with function body.
